c6c0741b7291a1f2d99124aaef936f4b0b93e855
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-04-05 16:47:07 -0300

    D-Pad Logic (Hat Axes) removed.

--- src/utils/VirtualGamepad.js
+++ src/utils/VirtualGamepad.js
@@ -196,23 +196,6 @@ export const updateGamepadState = (clientId, padIndex, state, padType) => {
     }
   });
 
-  // D-Pad Logic (Hat Axes)
-  /** @type {number} */
-  const hatX = (state.buttons[15]?.pressed ? 1 : 0) - (state.buttons[14]?.pressed ? 1 : 0);
-  /** @type {number} */
-  const hatY = (state.buttons[13]?.pressed ? 1 : 0) - (state.buttons[12]?.pressed ? 1 : 0);
-
-  if (hatX !== session.prevHatX) {
-    session.prevHatX = hatX;
-    uinput.emit(id, EV_ABS, keyCodes.ABS_HAT0X, hatX);
-    needsSync = true;
-  }
-  if (hatY !== session.prevHatY) {
-    session.prevHatY = hatY;
-    uinput.emit(id, EV_ABS, keyCodes.ABS_HAT0Y, hatY);
-    needsSync = true;
-  }
-
   // Analog Sticks
   AXIS_MAP.forEach((code, i) => {
     /** @type {number} */
